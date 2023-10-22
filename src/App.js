4511086615ec42b99ee52ef4c59244ac021ea06b
Tiago Garcez Ferrari <tiagoggferrari@gmail.com>
2023-10-21 21:34:46 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Louise te amo
+          Edit <code>src/App.js</code> and save to reload kk.
         </p>
         <a
           className="App-link"
