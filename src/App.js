b3a21d614d73a12171a303c5146cc807f1639a55
Tiago Garcez Ferrari <tiagoggferrari@gmail.com>
2023-10-23 22:18:53 -0300

    Ulitizando context para criar a classe search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Header from './Componentes/Header/header';
-import Search from './Componentes/Content/Search/search';
 import Content from './Componentes/Content/content';
 import Footer from './Componentes/Footer/footer';
+import { SearchContext, SearchProvider } from './Componentes/Search/search';
 
 function App() {
+
   return (
     <div>
-      <Header/>
-      <Search />
+      <Header />
+      <SearchContext.Provider />
       <Content />
       <Footer />
     </div>
---
+++ src/Componentes/Search/search.js
@@ -0,0 +1,35 @@
+//importa a função createContext e useState do pacote ‘react’
+import { createContext, useState } from "react";
+
+//Cria o contexto search
+export const SearchContext = createContext()
+
+//Cria um componente que aceita componentes filhos 
+export const SearchProvider = ({children}) => {
+    //Cria dois estados e uma função para atualizar cada estado
+    const [animesinfo, setInfoAnime] = useState([])
+    const [animeunico, setAnimeUnico] = useState([])
+  
+    //Cria uma função para buscar dados na API
+    const search = async (searchText) => {
+      const response = await fetch(
+            `https://api.myanimelist.net/v2/anime?q=${searchText}&limit=4`);
+        return await response.json();
+    }
+
+    //Retorna o componente que faz com que o valor do contexto esteja disponível para os childrens
+    return (
+        <SearchContext.Provider
+            //passa como value um objeto com os estados e funções
+            value = {{
+                search,
+                animesinfo,
+                setInfoAnime,
+                animeunico,
+                setAnimeUnico,
+            }}
+        >
+            {children}
+        </SearchContext.Provider>
+    )
+}
